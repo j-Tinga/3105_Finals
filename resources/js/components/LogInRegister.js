df0fdcc648c129e871532623c63947d536b6b55a
jackjk521 <jedchu541@gmail.com>
2021-12-10 00:27:15 +0800

    updated css and js for loginregister

--- resources/js/components/LogInRegister.js
+++ resources/js/components/LogInRegister.js
@@ -7,51 +7,41 @@ const LoginRegister = () => {
   const[addClass, setAddClass] = useState("");
 
   return (
-
-    <BrowserRouter>
-      <div className={`container ${addClass}`} id="container">
-      <div className="form-container sign-up-container">
-         < form  >
-           <h1>Create Account</h1>
-           <input type="text" placeholder="NAME" />
-           <input type="email" placeholder="EMAIL" />
-           <input type="password" placeholder="PASSWORD" />
-           <button type="submit"> <Link to ="/dashboard"> REGISTER </Link></button>
-         </form>
-       </div>
-      <div className="form-container sign-in-container">
-         <form>
-           <h1>Login</h1>
-           <input type="email" placeholder="EMAIL" />
-           <input type="password" placeholder="PASSWORD" />
-           <button type="submit"> <Link to ="/dashboard"> LOGIN </Link></button>
-         </form>
-       </div>
-      <div className="overlay-container">
-         <div className="overlay">
-           <div className="overlay-panel overlay-left">
-             <button className="ghost" id="signIn" onClick= {()=> setAddClass("")}>
-               GO TO LOGIN
-             </button>
-           </div>
-           <div className="overlay-panel overlay-right">
-             <button className="ghost" id="signUp" onClick= {()=> setAddClass("right-panel-active")}>
-                GO TO REGISTER
-             </button>
-           </div>
+        <div className= "body">
+          <div className={`container ${addClass}`} id="container">
+            <div className="form-container sign-up-container">
+              <form className="LRForm">
+                <h1 className="header1">Create Account</h1>
+                <input className ="inputBox" type="text" placeholder="NAME" />
+                <input className ="inputBox" type="email" placeholder="EMAIL" />
+                <input className ="inputBox" type="password" placeholder="PASSWORD" />
+                <button className="loginRegbuttons" type="submit"> REGISTER </button>
+              </form>
+            </div>
+            <div className="form-container sign-in-container">
+              <form className="LRForm">
+                <h1 className="header1">Login</h1>
+                <input className ="inputBox" type="email" placeholder="EMAIL" />
+                <input className ="inputBox" type="password" placeholder="PASSWORD" />
+                <button className="loginRegbuttons" type="submit"> LOGIN </button>
+              </form>
+            </div>
+            <div className="overlay-container">
+              <div className="overlay">
+                <div className="overlay-panel overlay-left">
+                  <button className="loginRegbuttons ghost" id="signIn" onClick= {()=> setAddClass("")}>
+                    GO TO LOGIN
+                  </button>
+                </div>
+                <div className="overlay-panel overlay-right">
+                  <button className="loginRegbuttons ghost" id="signUp" onClick= {()=> setAddClass("right-panel-active")}>
+                      GO TO REGISTER
+                  </button>
+                </div>
+                </div>
+            </div>
           </div>
-       </div>
-    </div>
-
-    <div>
-    <Routes>
-        <Route path='/dashboard' element= {<DashBoard/>}></Route>
-    </Routes>
-    </div>
-
-   
-    </BrowserRouter>
-  
+      </div>
     );
 };
 export default LoginRegister;
